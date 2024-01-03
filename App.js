a944550ba9e9b20514b07653597f3103b58f6602
beatriceToader <acribiebea@gmail.com>
2024-01-04 00:03:02 +0200

    New page and Google Sign In added

--- App.js
+++ App.js
@@ -1,12 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+import LoginScreen from './App/screens/LoginScreen';
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'Outfit-Regular': require('./assets/fonts/Outfit-Regular.ttf'),
+    'Outfit-Bold': require('./assets/fonts/Outfit-Bold.ttf'),
+    'Outfit-SemiBold': require('./assets/fonts/Outfit-SemiBold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <ClerkProvider publishableKey={'pk_test_c3BsZW5kaWQta2l3aS01Mi5jbGVyay5hY2NvdW50cy5kZXYk'}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+
+      <SignedIn>
+          <Text>You are Signed in</Text>
+      </SignedIn>
+
+      <SignedOut>
+          <LoginScreen/>
+      </SignedOut>
+      
     </View>
+    </ClerkProvider>
   );
 }
 
@@ -14,7 +47,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 25,
+  },
+  text:{
+    fontSize: 20,
+    fontFamily: 'Outfit-Regular'
   },
 });
---
+++ README.md
@@ -0,0 +1,4 @@
+# Event-Finder-App
+Get in touch with your new fun activity! We created a mobile app in order to make it easier for users to find new events based on their current location. The user creates an account and then the near events are displayed. You get access to the events' details and you can be redirected to the organizer's page to book a ticket.
+
+The link to our wireframe mockup for the application: https://app.moqups.com/O6a9sVNot9yqvWujzPs7WsOH89KRcL2b/view/page/a880590a1
